6f675bd811d37fd6be0e4f658a57ed050b03a632
Chris Salgado <musicman785@gmail.com>
2021-01-05 15:31:36 -0800

    Adds login to seasonDisplay.js file to show text and icons in browser

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,12 +1,34 @@
 import React from "react";
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the Beach!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, its chilly",
+        iconName: "snowflake"
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? "summer" : "winter";
+    } else {
+        return lat > 0 ? "winter" : "summer";
+    }
+};
+
+
 
 const SeasonDisplay = (props) => {
-
-
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
     return (
         <div>
-            <h1>{props.lat}</h1>
+            <i className={`${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`${iconName} icon`} />
         </div>
     )
 };
